fcedee95dde823d995eae8e701850d59f0706065
MiraclesRidiculed <140868803+MiraclesRidiculed@users.noreply.github.com>
2024-09-06 19:50:44 +0530

    nothing much learnt what ever had been changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ReactNode } from 'react';
-import { FaUser, FaRegBookmark, FaSearch, FaHome } from 'react-icons/fa';
-import { FaBuildingColumns } from 'react-icons/fa6';
-import Link from 'next/link';
-
 import localFont from 'next/font/local';
+import Taskbar from './components/taskbar';
 
 const mono = localFont({
 	src: 'mono.woff2',
@@ -18,47 +15,16 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-									   children,
-								   }: Readonly<{
+	children,
+}: Readonly<{
 	children: ReactNode;
 }>) {
 	return (
 		<html lang="en">
-		<body className={mono.className + ' bg-gray-950 text-blue-400'}>
-		{children}
-		<div className="flex justify-around p-4 fixed bottom-0 w-full z-10">
-			<Link
-				href={'/home'}
-				className="text-2xl p-3 transition-transform transform hover:scale-125"
-			>
-				<FaHome />
-			</Link>
-			<Link
-				href={'/resources'}
-				className="text-2xl p-3 transition-transform transform hover:scale-125"
-			>
-				<FaRegBookmark />
-			</Link>
-			<Link
-				href={'/search'}
-				className="text-2xl p-3 transition-transform transform hover:scale-125"
-			>
-				<FaSearch />
-			</Link>
-			<Link
-				href={'/campus'}
-				className="text-2xl p-3 transition-transform transform hover:scale-125"
-			>
-				<FaBuildingColumns />
-			</Link>
-			<Link
-				href={'/profile'}
-				className="text-2xl p-3 transition-transform transform hover:scale-125"
-			>
-				<FaUser />
-			</Link>
-		</div>
-		</body>
+			<body className={mono.className + ' bg-gray-950 text-blue-400'}>
+				{children}
+				<Taskbar /> {}
+			</body>
 		</html>
 	);
 }
